4764d6028bf2c2a783a82d95178bf7e994897d0d
alibeigi <alighb30@gmail.com>
2025-11-03 12:56:34 +0330

    wip: new branch #61

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ import 'core/tflite/face_recognizer.dart';
 import 'injection_container.dart' as di;
 
 void main() async {
-  // <--- تغییر: main async شد
   // اطمینان از مقداردهی اولیه bindings برای اجرای عملیات async
   WidgetsFlutterBinding.ensureInitialized();
 
